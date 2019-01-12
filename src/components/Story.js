fecff3c0c1ff51493555680cdcf7cd62acc6d824
Nishit Mohanan <nishitmohanan@gmail.com>
2019-01-13 00:12:44 +0530

    Updated render function to display stories

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,12 +1,38 @@
 import React from "react";
 
 class Story extends React.Component {
-  componentDidUpdate() {
-    console.log("updated!!!");
-  }
-
   render() {
-    return <li>{this.props.index}</li>;
+    let score;
+    if (this.props.post.score > 100) {
+      score = (
+        <span className="score score-highlight">{this.props.post.score}</span>
+      );
+    } else {
+      score = (
+        <span className="score score-normal">{this.props.post.score}</span>
+      );
+    }
+    return (
+      <li>
+        <a href={this.props.post.url} target="_blank" rel="noopener noreferrer">
+          <span>{this.props.post.title}</span>
+        </a>
+        <span className="hacker-news-link">
+          <a
+            href={this.props.post.hckr_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hacker News URL
+          </a>
+        </span>
+        <div>
+          {score} Points by {this.props.post.by}
+        </div>
+        <br />
+        <br />
+      </li>
+    );
   }
 }
 
