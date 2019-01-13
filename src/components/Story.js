f462b9925da3b1436ea5715a9e35d1f556447d30
Nishit Mohanan <nishitmohanan@gmail.com>
2019-01-13 23:06:33 +0530

    Changed  layout of the story component

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,47 +1,50 @@
 import React from "react";
 
 class Story extends React.Component {
+  getHostName = url => {
+    if (typeof url !== "undefined") {
+      return "(" + new URL(url).hostname + ")";
+    }
+    return "";
+  };
   render() {
-    let score;
-    let comments;
+    let score_class;
+    let comments_class;
     if (this.props.post.score > 100) {
-      score = (
-        <span className="score score-highlight">{this.props.post.score}</span>
-      );
+      score_class = "score-highlight";
     } else {
-      score = (
-        <span className="score score-normal">{this.props.post.score}</span>
-      );
+      score_class = "score-normal";
     }
     if (this.props.post.descendants > 100) {
-      comments = (
-        <span className="score score-highlight">{this.props.post.descendants}</span>
-      );
+      comments_class = "score-highlight";
     } else {
-      comments = (
-        <span className="score score-normal">{this.props.post.descendants}</span>
-      );
+      comments_class = "score-normal";
     }
-    
+
     return (
       <li>
         <a href={this.props.post.url} target="_blank" rel="noopener noreferrer">
-          <span>{this.props.post.title}</span>
+          <span>{this.props.post.title} </span>
         </a>
-        <span className="hacker-news-link">
+        <span className="hacker-news-link" />
+        <div className="hostname">{this.getHostName(this.props.post.url)}</div>
+        <div>
+          <span className={"score " + score_class}>
+            {this.props.post.score} Points
+          </span>
+          -
+          <span className={"score " + comments_class}>
+            {this.props.post.descendants} Comments
+          </span>
           <a
             href={this.props.post.hckr_url}
             target="_blank"
             rel="noopener noreferrer"
+            className="score hckr-news-url"
           >
-            Hacker News URL
+            (Hacker News URL)
           </a>
-        </span>
-        <div>
-          {score} Points by {this.props.post.by}
-          {comments} Comments
         </div>
-
         <br />
         <br />
       </li>
